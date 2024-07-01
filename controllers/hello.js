6d3703aca44311c7ee18a31d5bf31701befc9510
olakadasami <olakadasamuel@gmail.com>
2024-07-01 17:35:03 +0100

    change to using one api call

--- controllers/hello.js
+++ controllers/hello.js
@@ -4,31 +4,44 @@ exports.greet = async (req, res) => {
   try {
     const ip =
       req.headers["x-forwarded-for"] || req.socket.remoteAddress || req.ip;
+    console.log(ip);
 
-    // Request for IP info
-    const ipRes = await fetch(
-      `http://ip-api.com/json/${ip}?fields=status,message,city`
+    // Make request to get weather info
+    const weatherRes = await fetch(
+      `${weatherBaseUrl}/current.json?key=${process.env.WEATHER_API_KEY}&q=${ip}`
     );
-    const ipInfo = await ipRes.json();
+    const weatherInfo = await weatherRes.json();
 
-    if (ipInfo.data.status !== "fail") {
-      const city = ipInfo.data.city;
-      const weatherBaseUrl = "https://api.weatherapi.com/v1";
+    res.json({
+      client_ip: ip, // The IP address of the requester
+      location: city, // The city of the requester
+      greeting: `Hello, ${visitor}!, the temperature is ${weatherInfo.current.temp_c} degrees Celsius in ${weatherInfo.location.name}`,
+    });
 
-      // Make request to get weather info
-      const weatherRes = await fetch(
-        `${weatherBaseUrl}/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
-      );
-      const weatherInfo = await weatherRes.json();
+    // // Request for IP info
+    // const ipRes = await fetch(
+    //   `http://ip-api.com/json/${ip}?fields=status,message,city`
+    // );
+    // const ipInfo = await ipRes.json();
 
-      res.json({
-        client_ip: ip, // The IP address of the requester
-        location: city, // The city of the requester
-        greeting: `Hello, ${visitor}!, the temperature is ${weatherInfo.current.temp_c} degrees Celsius in ${weatherInfo.location.name}`,
-      });
-    } else {
-      res.status(404).json({ error: "Could not retrieve IP information" });
-    }
+    // if (ipInfo.data.status !== "fail") {
+    //   const city = ipInfo.data.city;
+    //   const weatherBaseUrl = "https://api.weatherapi.com/v1";
+
+    //   // Make request to get weather info
+    //   const weatherRes = await fetch(
+    //     `${weatherBaseUrl}/current.json?key=${process.env.WEATHER_API_KEY}&q=${city}`
+    //   );
+    //   const weatherInfo = await weatherRes.json();
+
+    //   res.json({
+    //     client_ip: ip, // The IP address of the requester
+    //     location: city, // The city of the requester
+    //     greeting: `Hello, ${visitor}!, the temperature is ${weatherInfo.current.temp_c} degrees Celsius in ${weatherInfo.location.name}`,
+    //   });
+    // } else {
+    //   res.status(404).json({ error: "Could not retrieve IP information" });
+    // }
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error" });
   }
