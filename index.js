dc67c4c09d8b3c162a89e42e6c7234f2d51f5b3e
olakadasami <olakadasamuel@gmail.com>
2024-07-01 17:54:56 +0100

    fix

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const helloRoutes = require("./routes/hello.route");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.set("trust proxy", true);
+
 app.use("/api/hello", helloRoutes);
 
 app.listen(PORT, () => {
